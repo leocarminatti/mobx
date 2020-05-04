9e0d0360ffb3465e7d9dee52a1d21dc54d0872d1
Leonardo Carminatti <l.carminatti@hotmail.com>
2020-05-04 18:13:41 -0300

    init

---
+++ todo_mobx/lib/stores/login_store.dart
@@ -0,0 +1,52 @@
+import 'package:mobx/mobx.dart';
+
+part 'login_store.g.dart';
+
+class LoginStore = _LoginStore with _$LoginStore;
+
+abstract class _LoginStore with Store {
+
+  @observable
+  String email = "";
+
+  @action
+  void setEmail(String value) => email = value;
+
+  @observable
+  String password = "";
+
+  @action
+  void setPassword(String value) => password = value;
+
+  @observable
+  bool loading = false;
+
+  @observable
+  bool passwordVisible = false;
+
+  @observable
+  bool loggedIn = false;
+
+  @action
+  void togglePasswordVisibility() => passwordVisible = !passwordVisible;
+
+  @computed
+  bool get isPasswordValid => password.length > 6;
+
+  @computed
+  bool get isEmailValid => email.length > 6;
+
+  @computed
+  Function get loginPressed =>
+      (isEmailValid && isPasswordValid && !loading) ? login : null;
+
+  @action
+  Future<void> login() async{
+    loading = true;
+
+    await Future.delayed(Duration(seconds: 2));
+
+    loading = false;
+    loggedIn = true;
+  }
+}
